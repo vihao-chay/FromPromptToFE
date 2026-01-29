73e7ad7f0a942306f779449a771f87f733385cea
Chu Phúc Minh Vượng <vuongcpmse180126@fpt.edu.vn>
2026-01-29 10:57:27 +0700

    Dựng src lần 2

--- FE/FromPromptToFE/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <div className="w-full py-6 text-center text-neutral-500 text-sm">
-            © 2026 AI CodeGen. All rights reserved.
-        </div>
-    )
-}
-
-export default Footer
